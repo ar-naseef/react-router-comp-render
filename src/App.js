fb850bf7ce45408d97e75067645a5b237f6dbbdd
naseef-ar <naseef14@gmail.com>
2020-07-30 06:27:04 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory
+} from "react-router-dom";
+import { EuiBottomBar, EuiButton } from '@elastic/eui'
+
+import '@elastic/eui/dist/eui_theme_light.css';
+
+function Home() {
+  return (
+    <div>Hi from Home!</div>
+  )
+}
+
+function About() {
+  return (
+    <div>Hi from About</div>
+  )
+}
+
+function Dashboard({ bottom }) {
+
+  console.log("bottom")
+  console.log(bottom)
+  const [bottomSheet, setBottomSheet] = useState(bottom ? true : false)
+  const history = useHistory()
+
+  return (
+    <div>
+      {bottomSheet ? (
+        <EuiBottomBar style={{
+          height: '70vh'
+        }}>
+          <div>
+            Hello all
+          </div>
+        </EuiBottomBar>
+      ) : null}
+      <div>Hello from Dash</div>
+      <EuiButton onClick={() => {
+        // setBottomSheet(true)
+        // history.push('/dashboard/bot')
+      }}>
+        <a href="/dashboard/bot">
+          Open bottom sheet
+        </a>
+      </EuiButton>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" exact>
+          <About />
+        </Route>
+        <Route path="/dashboard" exact>
+          <Dashboard />
+        </Route>
+        <Route path="/dashboard/bot" exact>
+          <Dashboard bottom={true} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
